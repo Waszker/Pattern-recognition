6f2a16ccacb55e3eb7088d4b000b35ffb3d42534
Piotr Waszkiewicz <waszka23@gmail.com>
2016-01-28 22:30:46 +0100

    Added value set normalization

--- Python/loading.py
+++ Python/loading.py
@@ -9,6 +9,21 @@ def _normalize_matrix(matrix):
     return matrix / row_sums[:, numpy.newaxis]
 #    return normalize(matrix, norm='l1', axis = 1)
 
+def get_normalize_vector():
+    """
+    Returns vector used in normalization that is extracted
+    from all numbers in data set.
+    """
+    for i in range(0, 10):
+        [train, test] = load_number_set(i)
+        if i == 0:
+            all_numbers = numpy.concatenate((train, test), axis = 0)
+        else:
+            all_numbers = numpy.concatenate((all_numbers, train, test), axis = 0)
+    normalized_vector =  numpy.amax(all_numbers, axis = 0)
+    normalized_vector[normalized_vector == 0] = 0.1 # Changes all occurences of zero to small value
+    return normalized_vector
+
 
 def load_number_set(number_to_load, division_ratio = 0.5):
     """Loads and returns two sets from number set"""
@@ -29,12 +44,37 @@ def load_number_set(number_to_load, division_ratio = 0.5):
     return [train, test]
 
 
-def load_letter_set(letter_to_load = '0'):
+def load_normalized_number_set(max_elems, number_to_load, division_ratio = 0.5):
+    numbers = load_number_set(number_to_load, division_ratio)
+    normalized_matrix = numbers / max_elems[None, :]
+    return normalized_matrix
+
+
+def load_letter_set(set_to_load = 0, letter_to_load = '0'):
+    """
+    Loads certain letters from letters set and returns them.
+    If letter_to_load is '0' then all letters will be loaded.
+    If set_to_load is 0 then the letters will be combined from
+    all two alternative letter sets.
+    """
     data = list()
-    reader = csv.reader(open('letters.csv', 'r'), delimiter=',')
+    set_filenames = { 0: 'letters.csv', 1 : 'letter_set_1.csv', 2 : 'letter_set_2.csv' }
+    reader = csv.reader(open(set_filenames[set_to_load], 'r'), delimiter=',')
     for row in reader:
         if str(letter_to_load) == '0' or str(row[0]) == str(letter_to_load):
             del row[0]
             data.append(row)
 
     return numpy.array(data).astype('double')
+
+def load_normalized_letter_set(max_elems, set_to_load = 0, letter_to_load = '0'):
+    """
+    Loads selected letter from certain letters set. See load_letter_set
+    method for some parameters explanation. This matrix is also normalized
+    using provided vector of maximal column values.
+    max_elems should be numpy vertical vector holding maximum values for
+    each column.
+    """
+    letters = load_letter_set(set_to_load, letter_to_load)
+    normalized_matrix = letters / max_elems[None, :]
+    return normalized_matrix
