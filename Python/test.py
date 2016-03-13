b44528568303fbab5b9d6fcb7632144e3229cd94
Anna Zawadzka <Anna Zawadzka>
2016-03-13 09:17:04 +0100

    new final results

--- Python/test.py
+++ Python/test.py
@@ -142,9 +142,9 @@ if __name__ == "__main__":
 #    sett, labels = e.getIdentifiedPoints(True)
 #    results = s.classifyPoints(sett, labels, gamma=0.125, normalize=True)
 #    np.savetxt('results_svm.txt', results, delimiter=',')
-#    results = k.classifyPoints(sett, labels, n=5, normalize=True)
+#    results = k.classifyPoints(sett, labels, n=4, normalize=True)
 #    np.savetxt('results_knn.txt', results, delimiter=',')
-#    results = rf.classifyPoints(sett, labels, trees=20, normalize=True)
+#    results = rf.classifyPoints(sett, labels, trees=100, normalize=True)
 #    np.savetxt('results_rf.txt', results, delimiter=',')
 
     for o, a in opts:
